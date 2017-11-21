6e2852d8e1e42070d349477473a5391dd2b19e9e
jerytang <jerytang@tencent.com>
2017-11-21 17:30:25 +0800

    添加 node 环境

--- image/03.sh
+++ image/03.sh
@@ -1,10 +1,10 @@
-# 安装node v6.10.1
+# 安装node v8.9.1
 RUN sudo apt-get update && sudo apt-get install -y wget
 
-# 使用淘宝镜像安装Node.js v6.10.1
-RUN wget https://npm.taobao.org/mirrors/node/v6.10.1/node-v6.10.1-linux-x64.tar.gz && \
-    tar -C /usr/local --strip-components 1 -xzf node-v6.10.1-linux-x64.tar.gz && \
-    rm node-v6.10.1-linux-x64.tar.gz 
+# 使用淘宝镜像安装Node.js v8.9.1
+RUN wget https://npm.taobao.org/mirrors/node/v8.9.1/node-v8.9.1-linux-x64.tar.gz && \
+    tar -C /usr/local --strip-components 1 -xzf node-v8.9.1-linux-x64.tar.gz && \
+    rm node-v8.9.1-linux-x64.tar.gz 
 
 # 将当前工作环境切换到 /node/app 目录，如果目录不存在，会自动创建
 WORKDIR /node/app
