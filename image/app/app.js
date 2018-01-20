fe1a9ff7da4f9e98cd985eef461cf69c7e3f3fb3
jinwei233 <jinwei233@github.com>
2018-01-20 13:34:03 +0800

    indent fix

--- image/app/app.js
+++ image/app/app.js
@@ -2,24 +2,24 @@ const http = require('http');
 const url = require('url');
 
 const {
-    readFile,
-    formula
+  readFile,
+  formula
 } = require('./lib/pdflatex');
 
 http.createServer(async function(req, res){
-    const request = url.parse(req.url, true);
-    const { query } = request;
-    let resp
-    try {
-        resp = await formula(query.f);
-    } catch (e) {
-        // console.log('=====');
-        // console.log(e.message);
-        // console.log('=====');
-        // res.setHeader("Latex-Error", new Buffer(e).toString('base64'));
-        res.setHeader("Latex-Error", e);
-        resp = await readFile('./img/error.png');
-    }
-    res.writeHead(200, {'Content-Type': 'image/png' });
-    res.end(resp, 'binary');
+  const request = url.parse(req.url, true);
+  const { query } = request;
+  let resp
+  try {
+    resp = await formula(query.f);
+  } catch (e) {
+    // console.log('=====');
+    // console.log(e.message);
+    // console.log('=====');
+    // res.setHeader("Latex-Error", new Buffer(e).toString('base64'));
+    res.setHeader("Latex-Error", e);
+    resp = await readFile('./img/error.png');
+  }
+  res.writeHead(200, {'Content-Type': 'image/png' });
+  res.end(resp, 'binary');
 }).listen(8002, '0.0.0.0');
--- image/app/lib/pdflatex.js
+++ image/app/lib/pdflatex.js
@@ -9,90 +9,90 @@ const execAsync = child_process.exec;
 var tmp = require('tmp');
 
 function createTmpDir() {
-    return new Promise(function(resolve, reject) {
-        tmp.dir(function (err, p) {
-            err ? reject(err) : resolve(p);
-        });
+  return new Promise(function(resolve, reject) {
+    tmp.dir(function (err, p) {
+      err ? reject(err) : resolve(p);
     });
+  });
 }
 
 function writeFile(path, data) {
-    return new Promise(function(resolve, reject) {
-        writeFileAsync(path, data, function(err) {
-            err ? reject(err) : resolve();
-        });
-    })
+  return new Promise(function(resolve, reject) {
+    writeFileAsync(path, data, function(err) {
+      err ? reject(err) : resolve();
+    });
+  })
 }
 
 function readFile(path) {
-    return new Promise(function(resolve, reject) {
-        readFileAsync(path, function(err, data) {
-            err ? reject(err) : resolve(data);
-        });
-    })
+  return new Promise(function(resolve, reject) {
+    readFileAsync(path, function(err, data) {
+      err ? reject(err) : resolve(data);
+    });
+  })
 }
 
 const isErrorLine = (line) => line.length > 0 && line.charAt(0) === '!';
 
 function filterErrorLines(log) {
-    return log
-        .split('\n')
-        .filter(isErrorLine)
-        .map(line => line.substring(2))
-        .join('')
+  return log
+    .split('\n')
+    .filter(isErrorLine)
+    .map(line => line.substring(2))
+    .join('')
 }
 
 async function readErrorLog(dirname) {
-    const log = await readFile(Path.join(dirname, 'texput.log'));
-    return filterErrorLines(log.toString()) || 'LaTeX Error';
+  const log = await readFile(Path.join(dirname, 'texput.log'));
+  return filterErrorLines(log.toString()) || 'LaTeX Error';
 }
 
 function exec(cmd, options = {}) {
-    return new Promise(function(resolve, reject) {
-        execAsync(cmd, options, function(err, stdout, stderr) {
-            err ? reject(err) : resolve({ stdout, stderr });
-        });
+  return new Promise(function(resolve, reject) {
+    execAsync(cmd, options, function(err, stdout, stderr) {
+      err ? reject(err) : resolve({ stdout, stderr });
     });
+  });
 }
 
 function createCommand() {
-    return [
-        'pdflatex',
-        '-shell-escape',
-        '-halt-on-error',
-        '-interaction=nonstopmode',
-        'texput.tex'
-    ].join(' ');
+  return [
+    'pdflatex',
+    '-shell-escape',
+    '-halt-on-error',
+    '-interaction=nonstopmode',
+    'texput.tex'
+  ].join(' ');
 }
 
 async function compile(tempPath, options = {}) {
-    try {
-        const cmd = createCommand();
-        await exec(cmd, {
-            ...options,
-            cwd: tempPath,
-            env: process.env,
-            timeout: 1000,
-        });
-        return readFile(Path.join(tempPath, 'texput.png')).then(data => {
-            // 移除临时目录
-            // TODO 有没有更加有效的方式，不开启 shell
-            exec('rm -r ' + tempPath);
-            return data;
-        });
-    } catch (e) {
-        throw await readErrorLog(tempPath);
-    }
+  try {
+    const cmd = createCommand();
+    await exec(cmd, {
+      ...options,
+      cwd: tempPath,
+      env: process.env,
+      timeout: 1000,
+    });
+    return readFile(Path.join(tempPath, 'texput.png')).then(data => {
+      // 移除临时目录
+      // TODO 有没有更加有效的方式，不开启 shell
+      exec('rm -r ' + tempPath);
+      return data;
+    });
+  } catch (e) {
+    throw await readErrorLog(tempPath);
+  }
 }
 
 async function pdflatex(source, options = {}) {
-    const tmpPath = await createTmpDir();
-    await writeFile(Path.join(tmpPath, 'texput.tex'), source);
-    return compile(tmpPath, options);
+  const tmpPath = await createTmpDir();
+  await writeFile(Path.join(tmpPath, 'texput.tex'), source);
+  return compile(tmpPath, options);
 }
 
 async function formula(eq) {
-    return await pdflatex(`
+  return await pdflatex(`
 \\documentclass[convert={density=600,outext=.png}]{standalone}
 \\begin{document}
 $ ${eq} $
